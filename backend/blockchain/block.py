8711d7030e414851771454c8de1bdc05fef65cb0
Rayhan Ferdous Srejon <srejonferdous@gmail.com>
2024-10-01 15:36:42 +0600

    Added difficulty and nonce values to the blockchain block

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -4,7 +4,9 @@ GENESIS_DATA = {
     'timestamp': 1,
     'last_hash': 'genesis_last_hash',
     'hash': 'genesis_hash',
-    'data': []
+    'data': [],
+    'difficulty': 3,
+    'nonce': 'genesis_nonce'
 
 }
 class Block:
@@ -12,11 +14,13 @@ class Block:
     Block: a unit of storage. 
     store transactions in a blockchain that supports a cryptocurrency.
     """
-    def __init__(self, timestamp, last_hash, hash, data):
+    def __init__(self, timestamp, last_hash, hash, data, difficulty, nonce):
         self.timestamp = timestamp
         self.last_hash = last_hash
         self.hash = hash
         self.data = data
+        self.difficulty = difficulty
+        self.nonce = nonce
     
     def __repr__(self):
         return(
@@ -24,19 +28,28 @@ class Block:
             f'timestamp: {self.timestamp}, '
             f'last_hash: {self.last_hash}, '
             f'hash: {self.hash}, '
-            f'data: {self.data})'
+            f'data: {self.data}), '
+            f'difficulty: {self.difficulty}), '
+            f'nonce: {self.nonce})'
         )
     
     @staticmethod
     def mine_block(last_block, data):
         """
-        Mine a block base on the given last_block and data. Returns
+        Mine a block based on the given last_block and data, until a block hash is found that meets the leading 0's proof of work requirement.
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        hash = crypto_hash(timestamp, last_hash, data)
-    
-        return Block(timestamp , last_hash, hash, data)
+        difficulty = last_block.difficulty
+        nonce = 0
+        hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
+
+        while hash[0:difficulty] != '0' * difficulty :
+            nonce += 1
+            timestamp = time.time_ns()
+            hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
+
+        return Block(timestamp, last_hash, hash, data, difficulty, nonce)
     
     @staticmethod
     def genesis():
@@ -48,6 +61,8 @@ class Block:
         #     last_hash = GENESIS_DATA['last_hash'],
         #     hash = GENESIS_DATA['hash'],
         #     data = GENESIS_DATA['data'],
+        #     difficulty = GENESIS_DATA['difficulty'],
+        #     nonce = GENESIS_DATA['nonce']
         # )
         return Block(**GENESIS_DATA)
 
