e13e2150280e4e88a9c7b0df14269c70b69e3bd7
Rayhan Ferdous Srejon <srejonferdous@gmail.com>
2024-10-05 02:07:33 +0600

    Added Block Validation in block.py

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,6 @@
-import time 
+import time
 from backend.util.crypto_hash import crypto_hash
-from backend.util.hex_to_binary import  hex_to_binary
+from backend.util.hex_to_binary import hex_to_binary
 from backend.config import MINE_RATE
 
 GENESIS_DATA = {
@@ -12,11 +12,14 @@ GENESIS_DATA = {
     'nonce': 'genesis_nonce'
 
 }
+
+
 class Block:
     """ 
     Block: a unit of storage. 
     store transactions in a blockchain that supports a cryptocurrency.
     """
+
     def __init__(self, timestamp, last_hash, hash, data, difficulty, nonce):
         self.timestamp = timestamp
         self.last_hash = last_hash
@@ -24,9 +27,9 @@ class Block:
         self.data = data
         self.difficulty = difficulty
         self.nonce = nonce
-    
+
     def __repr__(self):
-        return(
+        return (
             'Block('
             f'timestamp: {self.timestamp}, '
             f'last_hash: {self.last_hash}, '
@@ -35,7 +38,7 @@ class Block:
             f'difficulty: {self.difficulty}), '
             f'nonce: {self.nonce})'
         )
-    
+
     @staticmethod
     def mine_block(last_block, data):
         """
@@ -47,14 +50,14 @@ class Block:
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
-        while hex_to_binary(hash)[0:difficulty] != '0' * difficulty :
+        while hex_to_binary(hash)[0:difficulty] != '0' * difficulty:
             nonce += 1
             timestamp = time.time_ns()
             difficulty = Block.adjust_difficulty(last_block, timestamp)
             hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         return Block(timestamp, last_hash, hash, data, difficulty, nonce)
-    
+
     @staticmethod
     def genesis():
         """
@@ -77,24 +80,64 @@ class Block:
         Increase the difficulty for quickly mined blocks.
         Decrease the difficulty for slowly mined blocks.
         """
-        if(new_timestamp - last_block.timestamp) < MINE_RATE :
-           return last_block.difficulty + 1
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
 
-        if(last_block.difficulty - 1) > 0:
+        if (last_block.difficulty - 1) > 0:
             return last_block.difficulty - 1
 
         return 1
 
+    @staticmethod
+    def is_valid_block(last_block, block):
+        """
+        Validate the block bny enforcing the following rules:
+        - The block must have the proper last_hash reference
+        - The block must meet the Proof of Work requirement
+        - The difficulty must only adjust by one
+        _ The block hash must be a valid combination of the block fields
+        """
+
+        if block.last_hash != last_block.hash:
+            raise Exception('The Block last_hash must be correct.')
+
+        if hex_to_binary(block.hash)[0:block.difficulty] != '0' * block.difficulty:
+            raise Exception('The Proof of Work requirement was not met.')
+
+        if abs(last_block.difficulty - block.difficulty) > 1:
+            raise Exception('The block difficulty must only adjust by one.')
+
+        reconstructed_hash = crypto_hash(
+
+            block.timestamp,
+            block.last_hash,
+            # block.hash,
+            block.data,
+            block.nonce,
+            block.difficulty
+        )
+
+        if block.hash != reconstructed_hash:
+            raise Exception('The block hash must be correct')
+
 
 def main():
-
     # block = Block('foo')
     # print(block)
     # print(f'block.py__name__: {__name__}')
 
+    # genesis_block = Block.genesis()
+    # block = Block.mine_block(genesis_block, 'foo')
+    # print(block)
     genesis_block = Block.genesis()
-    block = Block.mine_block(genesis_block, 'foo')
-    print(block)
+    bad_block = Block.mine_block(Block.genesis(), 'foo')
+    bad_block.last_hash = 'evil_data'
+
+    try:
+        Block.is_valid_block(genesis_block, bad_block)
+    except Exception as e:
+        print(f'is valid block: {e}')
+
 
 
 if __name__ == '__main__':
